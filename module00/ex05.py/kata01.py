95ce9ef5729f435c470631da5da0210342aff943
Hajar LAMTAAI <72790103+minhajar@users.noreply.github.com>
2021-04-02 22:59:12 +0100

    Create kata01.py

---
+++ module00/ex05.py/kata01.py
@@ -0,0 +1,8 @@
+languages = {
+    'Python': 'Guido van Rossum',
+    'Ruby': 'Yukihiro Matsumoto',
+    'PHP': 'Rasmus Lerdorf',
+    }
+
+for key, value in range languages.items():
+    print(key,"was created by",value)
