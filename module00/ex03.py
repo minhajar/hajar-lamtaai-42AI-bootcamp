1b75b2c8692e16c93484b3267e0e1989fa8092b7
Hajar LAMTAAI <72790103+minhajar@users.noreply.github.com>
2021-04-02 21:48:35 +0100

    Update ex03.py

--- module00/ex03.py
+++ module00/ex03.py
@@ -1,1 +1,18 @@
+import string
+def text_analyzer(text):
+    p=len(text)
+    U=0
+    L=0
+    P=0
+    S=0
+    for i in range(p):
+        if text[i]==' ':
+            S+=1
+        if text[i].isupper()==True:
+            U+=1
+        if text[i].islower()==True:
+            L+=1
+        if text[i] in string.punctuation:
+            P+=1
+    return "This text contains :", p ,"caracters:", U,"upper letters",L,"lower letters",S ,"spaces", P ,"Punctuation marks" 
 
