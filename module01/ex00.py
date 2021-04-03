cff723b281608b3cc2907705fd66b1fdf156aef0
Hajar LAMTAAI <72790103+minhajar@users.noreply.github.com>
2021-04-03 18:11:21 +0100

    Update ex00.py

--- module01/ex00.py
+++ module01/ex00.py
@@ -16,12 +16,12 @@ class Recipe:
            return 'Recipe=(' + str(self.a) + ',' + self.b + ',' + self.c +',' + str(self.d) +',' + str(self.e)+',' + str(self.f)+')'
            
         
-        
+                
 class Book:
         x=' '
         y=0
         z=0
-        t=[]
+        t={'starter':[],'lunch':[],'dessert':[]}
         def __init__(self,book_name, last_update, creation_time, recipes_list):
             
             self.x=book_name
@@ -30,6 +30,20 @@ class Book:
             self.t=recipes_list
         def __str__ (self):
            return 'Book=(' + self.x + ',' + str(self.y) + ',' + str(self.z) +',' + str(self.t) +')'
+        def get_recipe_by_name(self,recipe_name):
+            return("the recipe's name is " + str(self.a))
+            pass
+        def get_recipes_by_type(self,recipe_type):
+            return self.t[self.f]
+            pass
+        def add_recipe(self, recipe):
+            recipe=Recipe(self.f)
+            for key in self.t:
+                 if self.t[key]==self.f:
+                     self.t[key].append(recipe.recipe_name)
+            pass
+                     
+                     
 #example    
 book1 = Book("pastery", 2020, 2002,["cake","cupcakes","popcakes"])
 print(book1)
